c1c433196785d5d362638e2c9e086ab58641fcf7
VaibhavJadaun <vt5407066@gmail.com>
2026-05-04 22:02:24 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ if (!cookieSecret) {
 }
 const app = express();
 //middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://majorproject-3-poq8.onrender.com", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(cookieSecret));
 //remove it in production
